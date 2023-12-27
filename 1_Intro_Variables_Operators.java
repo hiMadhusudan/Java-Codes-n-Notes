f8f7b4779fc46ea9ff3fdb3ac9177df9ae12fb92
Madhusudan Kumar <madhusudan.12004028@lpu.in>
2023-12-27 23:23:58 +0530

    initial commit

---
+++ 1_Intro_Variables_Operators.java
@@ -0,0 +1,16 @@
+/*
+ *  //Java Features:
+ *      -Simple and easy to learn
+ *      -Secure bcz of Java Execution Environment
+ *      -Portable: Bytecodes executed using JVM on mac, linux, windows
+ *
+ *  //Java Program Compilation:
+ *      java file -> compile using javac -> class file(bytecode)
+ * 
+ *  //Java Program Execution:
+ *      classloader loads class file -> bytecode verifier verifies bytecode and restrict illegal access to other parts of the system -> interpreter reads bytecode instruction and execute them line by line 
+ * 
+ * 
+ * 
+ * 
+ */
